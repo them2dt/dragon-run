a330926823a3af36385212d6efe24a8e00be79dc
ZombieGhostMG <zombieghostmg@gmail.com>
2023-05-22 15:16:34 +0100

    Fix animations warnings

--- src/components/phaser/PhaserGame.tsx
+++ src/components/phaser/PhaserGame.tsx
@@ -9,6 +9,8 @@ import eventsCenter from '@events-center';
 import EventKeys from '@consts/EventKeys';
 import SceneKeys from '@consts/SceneKeys';
 import OverlayKeys from '@consts/OverlayKeys';
+import TextureKeys from '@consts/TextureKeys';
+import AnimationKeys from '@consts/AnimationKeys';
 
 export default function PhaserGame() {
   const config: Phaser.Types.Core.GameConfig = {
@@ -33,7 +35,6 @@ export default function PhaserGame() {
   const { setOverlay } = useOverlay();
 
   useEffect(() => {
-    console.log('PhaserGame mounted');
     const phaserGame = new Phaser.Game(config);
 
     eventsCenter.on(EventKeys.GoToHome, () => {
@@ -41,7 +42,6 @@ export default function PhaserGame() {
       phaserGame.scene.stop(SceneKeys.CaveScene);
       phaserGame.sound.stopAll();
 
-      console.log('Starting HomeScene');
       phaserGame.scene.start(SceneKeys.HomeScene);
       setOverlay(OverlayKeys.Home);
     });
@@ -50,29 +50,28 @@ export default function PhaserGame() {
       phaserGame.scene.stop(SceneKeys.HomeScene);
       phaserGame.scene.stop(SceneKeys.GameOver);
       phaserGame.sound.stopAll();
-      console.log('Starting CaveScene');
+      const phaserAnimations: Phaser.Types.Animations.JSONAnimations = phaserGame.anims.toJSON();
+      phaserAnimations.anims.forEach((animation) => {
+        phaserGame.anims.remove(animation.key);
+      });
       phaserGame.scene.start(SceneKeys.CaveScene);
       setOverlay(OverlayKeys.Game);
     });
 
     eventsCenter.on(EventKeys.GoToGameOver, () => {
       if (phaserGame.scene.isActive(SceneKeys.GameOver)) {
-        console.log('GameOver is already active');
         return;
       }
-      console.log('Starting GameOver');
       phaserGame.scene.start(SceneKeys.GameOver);
       setOverlay(OverlayKeys.GameOver);
     });
 
     eventsCenter.on(EventKeys.PauseGame, () => {
-      console.log('Pausing CaveScene');
       phaserGame.scene.pause(SceneKeys.CaveScene);
       phaserGame.sound.pauseAll();
     });
 
     eventsCenter.on(EventKeys.ResumeGame, () => {
-      console.log('Resuming CaveScene');
       phaserGame.scene.resume(SceneKeys.CaveScene);
       phaserGame.sound.resumeAll();
     });
