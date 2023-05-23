bc6eab1e4a3ae7301472aab0137dfd1f77699ff9
ZombieGhostMG <zombieghostmg@gmail.com>
2023-05-23 17:02:50 +0100

    Env import fix?

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -2,7 +2,9 @@ import { FirebaseApp, initializeApp } from 'firebase/app';
 import 'firebase/firestore';
 import { Analytics, getAnalytics } from 'firebase/analytics';
 
-const clientCredentials = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG as unknown as string);
+const jsonClientCredentials = import.meta.env.VITE_FIREBASE_CONFIG as unknown as string;
+
+const clientCredentials = JSON.parse(jsonClientCredentials);
 
 const firebase: FirebaseApp = initializeApp(clientCredentials);
 const analytics: Analytics = getAnalytics(firebase);
