dd402ae13e41900282b4fa5ea275a593b00957c8
ZombieGhostMG <zombieghostmg@gmail.com>
2023-06-07 15:58:33 +0100

    Add homescreen music

--- src/phaser/scenes/HomeScene.ts
+++ src/phaser/scenes/HomeScene.ts
@@ -2,12 +2,20 @@ import * as Phaser from 'phaser';
 import SceneKeys from 'constants/SceneKeys';
 import eventsCenter from 'utils/eventsCenter';
 import EventKeys from 'constants/EventKeys';
+import MusicKeys from '@consts/audio/MusicKeys';
+import SoundFade from 'phaser3-rex-plugins/plugins/soundfade.js';
 
 export default class HomeScene extends Phaser.Scene {
+  public volume = 0.5;
+  public music!: Phaser.Sound.BaseSound;
+
   constructor() {
     super(SceneKeys.HomeScene);
   }
   create() {
+    this.music = this.sound.add(MusicKeys.HomeScene1, { loop: true });
+
+    SoundFade.fadeIn(this.music, 10000, 0.4);
     eventsCenter.emit(EventKeys.HomeLoaded);
   }
 }
