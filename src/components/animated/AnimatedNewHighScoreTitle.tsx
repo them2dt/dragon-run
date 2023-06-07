b69f29a4e967273a7a9a5fb05ee122c4c53f5c2d
ZombieGhostMG <zombieghostmg@gmail.com>
2023-06-07 18:09:38 +0100

    Change new highScore colour

--- src/components/animated/AnimatedNewHighScoreTitle.tsx
+++ src/components/animated/AnimatedNewHighScoreTitle.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import theme from '@consts/theme/theme';
 
 interface AnimatedNewHighScoreTitleProps {
   text: string;
@@ -10,8 +9,8 @@ interface AnimatedNewHighScoreTitleProps {
 const AnimatedNewHighScoreTitle = (props: AnimatedNewHighScoreTitleProps) => {
   return (
     <motion.h1
-      animate={{ color: [theme.colors.cB, theme.colors.cC, theme.colors.cB] }}
-      transition={{ duration: 1, repeat: Infinity, ease: 'linear' }}
+      animate={{ color: ['#a3e635', '#ffffff', '#ffffff', '#a3e635', '#a3e635', '#a3e635'] }}
+      transition={{ duration: 1, repeat: Infinity, ease: 'easeInOut' }}
       className={`py-4 text-2xl xs:text-2xl sm:text-3xl md:text-4xl ${props.className}`}
     >
       {props.text}
