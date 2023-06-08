97b846c3e2673254439eaf2226a1fad0ed4e14a0
ZombieGhostMG <zombieghostmg@gmail.com>
2023-06-08 10:03:42 +0100

    Improve home UI

--- src/overlays/Home.tsx
+++ src/overlays/Home.tsx
@@ -22,15 +22,13 @@ export default function Home() {
     <AnimatedPage>
       <OverlayWrapper className="bg-bg3 overflow-hidden">
         <Leaderboard leaderboardOpen={leaderboardOpen} closeLeaderboard={closeLeaderboard} />
-        <div className="w-full h-full m-auto flex flex-col max-w-[1240px]">
-          <div className="h-auto my-auto">
-            <img src={logo} alt="logo" className="m-auto lg:w-[600px] h-auto rendering-pixelated" />
-          </div>
-          <div className="grid grid-cols-2 gap-4 md:gap-6 p-4 mt-auto mb-0 lg:mb-8">
-            <AnimatedButton text="Store" className="m-auto w-full col-span-1 bg-cD md:text-2xl line-through" />
+        <div className="w-full mx-auto h-full flex flex-col max-w-[1240px]">
+          <img src={logo} alt="logo" className="m-auto lg:w-[600px] h-auto rendering-pixelated" />
+          <div className="grid grid-cols-2 gap-4 md:gap-6 p-4 mb-0 lg:mb-8">
+            <AnimatedButton text="Store" className=" w-full col-span-1 bg-cD md:text-2xl line-through" />
             <AnimatedButton
               text="Leaderboard"
-              className="m-auto w-full col-span-1 bg-cD md:text-2xl"
+              className=" w-full col-span-1 bg-cD md:text-2xl"
               onClick={openLeaderboard}
             />
             <AnimatedButton
