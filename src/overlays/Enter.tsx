a216235a9e3515798c00b61f85abe73a14473086
ZombieGhostMG <zombieghostmg@gmail.com>
2023-06-08 18:06:08 +0100

    Change enter button to MUI

--- src/overlays/Enter.tsx
+++ src/overlays/Enter.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import AnimatedButton from 'components/animated/AnimatedButton';
 import eventsCenter from 'utils/eventsCenter';
 import EventKeys from 'constants/EventKeys';
 import AnimatedPage from 'components/animated/AnimatedPage';
 import OverlayWrapper from 'components/OverlayWrapper';
 import AnimatedEnterTitle from 'components/animated/AnimatedEnterText';
+import { Button, Typography, useTheme } from '@mui/material';
 
 interface EnterProps {
   userName: string;
 }
 
 export default function Enter({ userName }: EnterProps) {
+  const muiTheme = useTheme();
   const handleEnterClick = () => {
     eventsCenter.emit(EventKeys.GoToHome);
   };
@@ -20,11 +21,30 @@ export default function Enter({ userName }: EnterProps) {
         <div className="w-full h-full m-auto flex flex-col max-w-[1240px] text-center">
           <div className="h-auto my-auto">
             <AnimatedEnterTitle userName={userName} />
-            <AnimatedButton
-              text="Enter"
-              className="m-auto w-auto py-3 px-5 md:py-5 md:px-10 text-4xl sm:text-5xl md:text-6xl"
+            <Button
+              variant="contained"
+              size="large"
+              sx={{
+                backgroundColor: muiTheme.palette.secondary.main,
+                '&:hover': {
+                  backgroundColor: muiTheme.palette.text.secondary,
+                  color: muiTheme.palette.secondary.main,
+                },
+                width: '200px',
+                height: '70px',
+                [muiTheme.breakpoints.up('sm')]: { width: '300px', height: '80px' },
+                [muiTheme.breakpoints.up('md')]: { width: '500px', height: '100px' },
+              }}
               onClick={() => handleEnterClick()}
-            />
+            >
+              <Typography
+                sx={{ fontSize: '2.2rem', [muiTheme.breakpoints.up('md')]: { fontSize: '3rem' } }}
+                fontWeight={400}
+                component="h3"
+              >
+                Play
+              </Typography>
+            </Button>
           </div>
         </div>
       </OverlayWrapper>
