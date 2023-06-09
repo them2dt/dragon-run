e3bfbec9d6f2097713a993ad672580bcc29e318a
ZombieGhostMG <zombieghostmg@gmail.com>
2023-06-09 17:15:17 +0100

    Add home socials

--- src/components/HomeNavBar.tsx
+++ src/components/HomeNavBar.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Box, IconButton, useTheme } from '@mui/material';
 import { Settings } from '@mui/icons-material';
+import { discord, twitter } from '@consts/Socials';
+import { FaDiscord, FaTwitter } from 'react-icons/fa';
 
 interface HomeNavBarProps {
   openSettings: () => void;
@@ -12,6 +14,12 @@ export default function HomeNavBar({ openSettings }: HomeNavBarProps) {
     <Box className="fixed left-[5px] top-[2px]">
       <IconButton aria-label="settings" onClick={openSettings}>
         <Settings sx={{ fill: muiTheme.palette.text.secondary }} fontSize="large" />
+      </IconButton>
+      <IconButton onClick={() => window.open(twitter, '_blank')} size="large">
+        <FaTwitter color="#1C98E5" />
+      </IconButton>
+      <IconButton onClick={() => window.open(discord, '_blank')} size="large">
+        <FaDiscord color="#5460E6" />
       </IconButton>
     </Box>
   );
