fcc652797bc6aeb853723aca1f08259e1b89388a
ZombieGhostMG <zombieghostmg@gmail.com>
2023-07-16 14:01:30 +0100

    Game loading scroll to top

--- src/components/PreGameDialog.tsx
+++ src/components/PreGameDialog.tsx
@@ -40,6 +40,7 @@ export default function PreGameDialog({ preGameOpen, closePreGame, openShop }: P
   };
 
   const handleLoading = () => {
+    scrollToTop();
     handleOpenScreen(PreGameScreens.Loading);
   };
 
