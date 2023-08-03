fabb759c90f8bfb056ae721391b8582c8342e03a
ZombieGhostMG <zombieghostmg@gmail.com>
2023-08-03 21:40:38 +0100

    Add alert copy function

--- src/components/alerts/Alert.tsx
+++ src/components/alerts/Alert.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import { Alert, Snackbar } from "@mui/material";
+import React, { useState } from "react";
+import { Alert, Snackbar, ClickAwayListener, Tooltip, IconButton } from "@mui/material";
 import { useAlert } from "@context/useAlert";
+import { useTheme } from "@mui/material/styles";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
 export default function BottomAlert() {
   const { alert, setAlert } = useAlert();
+  const muiTheme = useTheme();
 
-  const { open, message, severity, hideDuration } = alert;
+  const { open, message, severity, hideDuration, copyText } = alert;
 
   const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
     if (reason === "clickaway") {
@@ -18,10 +21,56 @@ export default function BottomAlert() {
     });
   };
 
+  const [textCopied, setTextCopied] = useState(false);
+
+  const handleTCopyText = () => {
+    if (copyText === undefined) {
+      return;
+    }
+    navigator.clipboard.writeText(copyText).catch((err) => {
+      console.error("Failed to copy to clipboard: ", err);
+    });
+    setTextCopied(true);
+    setTimeout(() => {
+      setTextCopied(false);
+    }, 1000);
+  };
+
+  const handleTextCopyReset = () => {
+    setTextCopied(false);
+  };
+
   return (
-    <Snackbar open={open} autoHideDuration={hideDuration ?? 3000} onClose={handleClose}>
+    <Snackbar open={open} autoHideDuration={hideDuration} onClose={handleClose}>
       <Alert variant="filled" onClose={handleClose} severity={severity} sx={{ width: "100%", borderRadius: 0 }}>
         {message}
+        {copyText !== "" && (
+          <ClickAwayListener onClickAway={handleTextCopyReset}>
+            <div>
+              <Tooltip
+                PopperProps={{
+                  disablePortal: true
+                }}
+                onClose={handleTextCopyReset}
+                open={textCopied}
+                disableFocusListener
+                disableHoverListener
+                disableTouchListener
+                title="Copied!"
+              >
+                <IconButton
+                  onClick={() => {
+                    handleTCopyText();
+                  }}
+                  size="large"
+                  sx={{ "&:hover": { backgroundColor: muiTheme.palette.background.light } }}
+                >
+                  <ContentCopyIcon sx={{ fill: muiTheme.palette.text.secondary }} fontSize="small" />
+                </IconButton>
+              </Tooltip>
+            </div>
+          </ClickAwayListener>
+        )}
       </Alert>
     </Snackbar>
   );
--- src/context/AlertProvider.tsx
+++ src/context/AlertProvider.tsx
@@ -1,17 +1,18 @@
-import type AlertSeverityKeys from "@consts/AlertSeverityKeys";
+import AlertSeverityKeys from "@consts/AlertSeverityKeys";
 import React, { createContext, useState } from "react";
 
 export interface Alert {
   open: boolean;
   message: string;
-  severity: AlertSeverityKeys | undefined;
-  hideDuration?: number | null;
+  severity: AlertSeverityKeys;
+  hideDuration?: number;
+  copyText?: string;
 }
 
 interface AlertContextType {
   alert: Alert;
   setAlert: React.Dispatch<React.SetStateAction<Alert>>;
-  newAlert: (message: string, severity: AlertSeverityKeys, hideDuration?: number | null) => void;
+  newAlert: (message: string, severity: AlertSeverityKeys, hideDuration?: number) => void;
 }
 
 interface AlertProviderProps {
@@ -24,16 +25,24 @@ export const AlertProvider = ({ children }: AlertProviderProps) => {
   const [alert, setAlert] = useState<Alert>({
     open: false,
     message: "",
-    severity: undefined,
-    hideDuration: null
+    severity: AlertSeverityKeys.Info,
+    hideDuration: 3000,
+    copyText: ""
   });
 
-  const newAlert = (message: string, severity: AlertSeverityKeys, hideDuration?: number | null) => {
+  const newAlert = (message: string, severity: AlertSeverityKeys, hideDuration?: number, copyText?: string) => {
+    if (hideDuration === undefined) {
+      hideDuration = 3000;
+    }
+    if (copyText === undefined) {
+      copyText = "";
+    }
     setAlert({
       open: true,
       message,
       severity,
-      hideDuration
+      hideDuration,
+      copyText
     });
   };
 
